d5ffec27c22ddc9f368595251ec1abae271c8daf
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-03-29 21:53:10 +0100

    fonts installation added to install script

--- install_fedora.sh
+++ install_fedora.sh
@@ -14,9 +14,20 @@ ln -s "$DIR/.oh-my-zsh" ~/.
 ln -s "$DIR/.gitconfig.linux.private" ~/.gitconfig
 ln -s "$DIR/tools" ~/
 
+#install fonts
+mkdir ~/.fonts
+cp -rv "$DIR/fonts" ~/.fonts
+fc-cache
+
+#fetch dependencies
 cd $DIR
 git submodule init
 git submodule update
 
+#set solarized scheme
 $DIR/gnome-terminal-colors-solarized/install.sh
+
+#setting colors to solarized should result in the default proile with the following id set
+#TODO: choose the profile dynamically
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font "'Inconsolata for Powerline Medium 18'"
 
