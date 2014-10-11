3ddb6a7bb7f1065a00f6780dcf1fdce5a48a7140
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-10-11 17:41:49 +0200

    gajim in default ubuntu install ; added coarse pink abs slic3r setting

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get dist-ugprade
-sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz
+sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/install_common.sh
