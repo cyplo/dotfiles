3d6e15d0109b2e7683488ca30d478d6ef16bc19d
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-10-11 10:58:07 +0200

    better customizer for the fresh ubuntu; fixed rvm initial install problem

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get dist-ugprade
-sudo apt-get install meld whois zsh tmux
+sudo apt-get install meld whois zsh tmux vim atop aria2 curl
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/install_common.sh
 
