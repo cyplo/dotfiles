718bb9c1814d1d71f5a71653f5e0dc6c86db8bac
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-10-11 11:25:24 +0200

    sdcard backup tool added

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get dist-ugprade
-sudo apt-get install meld whois zsh tmux vim atop aria2 curl
+sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/install_common.sh
---
+++ tools/backup_sdcard.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "usage: $0 device_to_clone"
+    exit
+fi
+
+device=$1
+
+timestamp=`date +%Y%M%d`
+dest_file="/tmp/$timestamp.dd.xz"
+
+echo "about to clone $device to $dest_file"
+echo "ctrl-c or [enter]"
+read
+
+umount $device?
+umount $device
+
+sudo pv -tpreb $device | dd bs=4M | pixz > $dest_file
+
