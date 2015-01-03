731436d5aa8a41ff7794c81a32e69747cb4cc9e5
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-01-03 16:34:10 +0100

    pip added to the default install

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get full-upgrade
-sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol
+sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol python-pip
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/install_common.sh
