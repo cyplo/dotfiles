ddc9fa70e3469be9962df0d44d8c7790558012d6
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-01-03 17:42:37 +0100

    intel/nvidia optimus support for ubuntu

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get full-upgrade
-sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol python-pip
+sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol python-pip mesa-utils mesa-utils-extra
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/install_common.sh
