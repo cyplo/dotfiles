90645cfca51ea1c83cdde32729cdcf81789f9ca5
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-01-03 15:42:17 +0100

    terminator as default terminal emulator udner ubuntu

--- install_common.sh
+++ install_common.sh
@@ -22,8 +22,8 @@ ln -vfs "$DIR/.gitconfig.linux.private" ~/.gitconfig
 ln -vfs "$DIR/tools" ~/
 
 # stuff that does not like symbolic links
+mkdir -vp ~/.config/terminator
 rm -f ~/.config/terminator/config
-mkdir -p "~/.config/terminator"
 ln "$DIR/.config/terminator/config" ~/.config/terminator/config
 
 #install fonts
@@ -32,6 +32,7 @@ cp -rv "$DIR/fonts" ~/.fonts
 fc-cache
 
 #rvm
+gpg --recv-keys BF04FF17
 curl -sSL https://get.rvm.io | bash -s stable
 source $HOME/.rvm/scripts/rvm
 rvm install ruby
@@ -39,16 +40,6 @@ rvm install ruby
 # talks: reveal-md
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g reveal-md
-
-#set solarized scheme
-$DIR/gnome-terminal-colors-solarized/install.sh
-
-#setting colors to solarized should result in the default proile with the following id set
-#TODO: choose the profile dynamically
-dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font "'Inconsolata for Powerline Medium 18'"
-dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/login-shell true
-dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/audible-bell false
-dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-system-font false
 
 echo
 echo "now go ahead and restart Gnome session"
