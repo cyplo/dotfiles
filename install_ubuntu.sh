87a4b6760f0bf12e6fadc3e9c27e28e76adfbb41
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-01-03 16:28:58 +0100

    gsmartcontrol utility added

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get full-upgrade
-sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator
+sudo apt-get install meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/install_common.sh
