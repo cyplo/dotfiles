b7de54185c3b312b882a6f70da669d9cd3d9c0e9
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-01-06 10:58:14 +0100

    mercurial added to the list of default ubuntu tools

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol python-pip mesa-utils mesa-utils-extra aptitude p7zip-full p7zip-rar thunderbird
+sudo apt-get install -y meld whois zsh tmux vim atop aria2 curl pv pixz gajim tor torsocks nodejs npm terminator gsmartcontrol python-pip mesa-utils mesa-utils-extra aptitude p7zip-full p7zip-rar thunderbird mercurial
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/install_common.sh
