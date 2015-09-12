3bb2c2e8304f6b5057f8e2f4f901e11cde654b53
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2015-09-12 07:48:45 +0200

    use dnf isntead of youm to install kicad on fedora from sources

--- fedora/install_kicad_fedora.sh
+++ fedora/install_kicad_fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum groupinstall "C Development Tools and Libraries" 
-sudo yum install bzr bzrtools cmake GLC_lib-devel glew-devel cairo-devel bzip2-devel wxGTK-devel openssl-devel doxygen patch
+sudo dnf groupinstall "C Development Tools and Libraries" 
+sudo dnf install bzr bzrtools cmake GLC_lib-devel glew-devel cairo-devel bzip2-devel wxGTK3-devel wxGTK-devel openssl-devel doxygen patch
 ../common/install_kicad_from_sources
 
 
