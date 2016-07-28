f6374ee0c484c8b2362699ab2c6c7bb39f14382d
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-28 11:09:53 +0200

    add basic scaffolding around automatic build for windows on appveyor

---
+++ windows_cygwin/build.bat
@@ -0,0 +1,3 @@
+REM install Chocolatey
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
