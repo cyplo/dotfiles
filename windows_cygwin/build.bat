ad8258d263fd558d7851b101459a6b81c05f228f
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-28 11:17:16 +0200

    add browsers and cygwin to the default windows installation

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -1,3 +1,9 @@
 REM install Chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
+REM install base tools
+choco install firefox
+choco install googlechrome
+
+REM install cygwin and all things inside
+choco install cygwin cyg-get
