66e6940246202d5e7ef37d7e75ec148ee9fc15d4
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 12:28:40 +0200

    do not use cygwin from chocolatey, have more control

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -1,4 +1,6 @@
-choco install -y firefox googlechrome
-choco install -fy cygwin --installargs "-Pg curl,zsh,git,vim,wget"
+choco install -y firefox googlechrome wget
 refreshenv
+wget https://cygwin.com/setup-x86_64.exe
+REM cygwin installation
+setup-x86_64 -R c:\cywin\ -Pg curl,zsh,git,vim,wget
 
