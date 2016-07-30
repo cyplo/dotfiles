9ab763e98886a37fdda15bff953f6d49acdc36d3
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 21:05:47 +0200

    link in the already downloaded repo on windows

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -2,4 +2,6 @@ choco install -y firefox googlechrome wget
 wget -c https://cygwin.com/setup-x86_64.exe
 REM cygwin installation
 setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion
+c:\cygwin64\bin\bash.exe --login -c "mkdir -v $HOME/dev/"
+c:\cygwin64\bin\bash.exe --login -c "ln -vfs `cygpath $HOMEPATH`/dev/dotfiles $HOME/dev"
 
