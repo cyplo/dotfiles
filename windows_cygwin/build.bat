cde5063db579865996aeb3d1cca4a49081e6dd78
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 21:30:37 +0200

    tell git explicitely where to clone the inner repo

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -5,7 +5,7 @@ setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh
 
 c:\cygwin64\bin\bash.exe --login -c "cp -vr `cygpath $HOMEPATH`/.ssh $HOME/"
 c:\cygwin64\bin\bash.exe --login -c "mkdir -pv $HOME/dev/"
-c:\cygwin64\bin\bash.exe --login -c "git clone git@github.com:cyplo/dotfiles.git"
+c:\cygwin64\bin\bash.exe --login -c "git clone git@github.com:cyplo/dotfiles.git dev/dotfiles"
 
 c:\cygwin64\bin\bash.exe --login -c "export DIR=$HOME/dev/dotfiles && bash $DIR/common/configure_fresh_system"
 
