bc0067654deb5e8ce89443355bf1b6297cd61609
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 12:38:29 +0200

    continue downloading cygwin's setup if interrupted or running once again

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -1,5 +1,5 @@
 choco install -y firefox googlechrome wget
-wget https://cygwin.com/setup-x86_64.exe
+wget -c https://cygwin.com/setup-x86_64.exe
 REM cygwin installation
 setup-x86_64 -R "C:\cygwin" -Pg curl,zsh,git,vim,wget
 
