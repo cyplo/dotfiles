d220ff776d444f7f33af6a36ca008909eb5bb15c
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 21:16:44 +0200

    invoke common configure script from the windows build

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -4,4 +4,5 @@ REM cygwin installation
 setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion
 c:\cygwin64\bin\bash.exe --login -c "mkdir -pv $HOME/dev/"
 c:\cygwin64\bin\bash.exe --login -c "ln -vfs `cygpath $HOMEPATH`/dev/dotfiles $HOME/dev"
+c:\cygwin64\bin\bash.exe --login -c "DIR=$HOME/dev/dotfiles $DIR/common/configure_fresh_system"
 
