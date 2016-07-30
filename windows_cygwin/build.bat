35b0a5db637c2c8f16a2dc1e84203d0be19a96f2
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 21:24:10 +0200

    do not link in the repo from windows under cygwin, clone a new one

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -1,8 +1,11 @@
-choco install -y firefox googlechrome wget
+choco install -y firefox googlechrome wget notepadplusplus
+
 wget -c https://cygwin.com/setup-x86_64.exe
-REM cygwin installation
 setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion
+
+c:\cygwin64\bin\bash.exe --login -c "cp -vr `cygpath $HOMEPATH`/.ssh $HOME/"
 c:\cygwin64\bin\bash.exe --login -c "mkdir -pv $HOME/dev/"
-c:\cygwin64\bin\bash.exe --login -c "ln -vfs `cygpath $HOMEPATH`/dev/dotfiles $HOME/dev"
-c:\cygwin64\bin\bash.exe --login -c "DIR=$HOME/dev/dotfiles $DIR/common/configure_fresh_system"
+c:\cygwin64\bin\bash.exe --login -c "git clone git@github.com:cyplo/dotfiles.git"
+
+c:\cygwin64\bin\bash.exe --login -c "export DIR=$HOME/dev/dotfiles && bash $DIR/common/configure_fresh_system"
 
