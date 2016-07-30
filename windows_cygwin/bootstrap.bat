9105dff9eecf28c575a718d8a0bd9de23edf14a9
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-07-30 10:53:09 +0200

    echo commands on screen in windows bootstrap

--- windows_cygwin/bootstrap.bat
+++ windows_cygwin/bootstrap.bat
@@ -1,4 +1,4 @@
+echo on
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-refreshenv
 choco install -y git win32-openssh
 
