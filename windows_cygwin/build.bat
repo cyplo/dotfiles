cbd0838506a857bf8c6e2b984cf6770491b7caf6
Cyryl Płotnicki-Chudyk <cyplo@cyplo.net>
2016-08-04 11:21:31 +0200

    separated the insider script for in-cygwin execution

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -4,16 +4,11 @@ choco install -y firefox googlechrome wget notepadplusplus sublimetext3
 wget -c https://cygwin.com/setup-x86_64.exe
 setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion,zlib,fontconfig
 
+set script_path=%~dp0
+set repo_path=%script_path%\..\
 set bash=c:\cygwin64\bin\bash.exe --login -c
+
 for /f "delims=" %%A in ('%bash% "cd `cygpath $HOMEPATH`/dev/dotfiles && git rev-parse --abbrev-ref HEAD"') do set "branch=%%A" 
 %bash% 'echo "branch is $branch"'
 %bash% "cp -vr `cygpath $HOMEPATH`/.ssh $HOME/"
-
-%bash% "mkdir -pv $HOME/dev/"
-%bash% "rm -fr $HOME/dev/dotfiles"
-%bash% "git clone `cygpath $HOMEPATH`/dev/dotfiles $HOME/dev/dotfiles"
-%bash% "cd $HOME/dev/dotfiles && git remote set-url origin git@github.com:cyplo/dotfiles.git"
-%bash% "cd $HOME/dev/dotfiles && git checkout $branch"
-%bash% "cd $HOME/dev/dotfiles && git pull"
-%bash% "export DIR=$HOME/dev/dotfiles && NOSUDO=true DONT_CHANGE_SHELL=true NORUST=true $DIR/common/configure_fresh_system"
-
+%bash% "export OUTER_CLONE=`cygpath $repo_path` && $script_path/build_insider.sh"
---
+++ windows_cygwin/build_insider.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOTFILES_PATH="$HOME/dev/dotfiles"
+mkdir -pv $HOME/dev/
+rm -fr $DOTFILES_PATH
+
+git clone "$OUTER_CLONE" "$DOTFILES_PATH"
+cd "$DOTFILES_PATH"
+git remote set-url origin git@github.com:cyplo/dotfiles.git
+git checkout $branch
+git pull
+export DIR=$DOTFILES_PATH && NOSUDO=true DONT_CHANGE_SHELL=true NORUST=true $DIR/common/configure_fresh_system"
+
