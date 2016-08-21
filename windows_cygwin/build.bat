2404e42f1c38145c152db1c9ea11ac75bad5cedc
Cyryl Płotnicki <cyplo@cyplo.net>
2016-08-21 18:39:05 +0200

    Explicitly install all packages on windows via choco

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -1,5 +1,5 @@
 @echo on
-choco install -y firefox googlechrome wget notepadplusplus sublimetext3 mono monodevelop gtksharp visualstudiocode microsoft-build-tools visualstudio2015community p4merge dotnet4.5 ruby nodejs
+choco install -y --allowEmptyChecksum firefox googlechrome wget notepadplusplus sublimetext3 mono monodevelop gtksharp visualstudiocode microsoft-build-tools visualstudio2015community p4merge dotnet4.5 ruby nodejs
 
 wget -c https://cygwin.com/setup-x86_64.exe
 setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion,zlib,fontconfig,clang,cmake,lua,perl
