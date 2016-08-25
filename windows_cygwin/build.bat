017b5323515ade00846c73690741122e9376e6e2
Cyryl Płotnicki <cyplo@cyplo.net>
2016-08-25 15:37:05 +0200

    Add `ag` to cygwin

--- windows_cygwin/build.bat
+++ windows_cygwin/build.bat
@@ -2,7 +2,7 @@
 choco install -y --allowEmptyChecksum firefox googlechrome wget notepadplusplus sublimetext3 mono monodevelop gtksharp visualstudiocode microsoft-build-tools visualstudio2015community p4merge dotnet4.5 ruby nodejs stylecop
 
 wget -c https://cygwin.com/setup-x86_64.exe
-setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion,zlib,fontconfig,clang,cmake,lua,perl
+setup-x86_64.exe -R "C:\cygwin64" -s http://cygwin.netbet.org/ -q -g -P curl,zsh,git,vim,wget,xz,tar,gawk,bzip2,subversion,zlib,fontconfig,clang,cmake,lua,perl,the_silver_searcher
 
 set script_path=%~dp0
 set repo_path=%script_path%\..\
