febac508e8cb94133b9746b4660b7933fc275e06
Cyryl Płotnicki <cyplo@cyplo.net>
2017-01-09 10:28:02 +0000

    Hyper: solarized dark

--- .hyper.js
+++ .hyper.js
@@ -91,7 +91,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+               'hyper-solarized-dark'
+           ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
