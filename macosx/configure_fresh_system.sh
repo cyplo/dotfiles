2ecf4bbf4f039efb757d286062a3c94a0a9eff31
Cyryl Płotnicki <cyplo@cyplo.net>
2017-04-09 10:15:09 +0100

    Try unlinking gpg2 if needed

--- macosx/configure_fresh_system.sh
+++ macosx/configure_fresh_system.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 export PATH="/usr/local/bin:$PATH"
 
+brew unlink gnupg2
 set -e
 
 brew update
