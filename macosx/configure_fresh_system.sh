8adc78847cc011b50a130d3b26f21b9efc10bc00
Cyryl Płotnicki <cyplo@cyplo.net>
2017-08-04 21:23:27 +0200

    Force gpg2 upgrade before others

--- macosx/configure_fresh_system.sh
+++ macosx/configure_fresh_system.sh
@@ -5,8 +5,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 export PATH="/usr/local/bin:$PATH"
 
-set -e
 set -x
+
+set +e
+brew unlink gnupg
+brew unlink gnupg2
+brew uninstall gnupg
+brew uninstall gnupg2
+brew unlink dirmngr
+brew uninstall dirmngr
+set -e
+
+brew install --force gnupg2
 
 brew update
 brew upgrade
@@ -23,16 +33,6 @@ brew install \
   tree \
   watch \
   zsh
-
-set +e
-brew unlink gnupg
-brew unlink gnupg2
-brew uninstall gnupg
-brew uninstall gnupg2
-brew unlink dirmngr
-brew uninstall dirmngr
-set -e
-brew install --force gnupg2
 
 brew install \
   coreutils \
