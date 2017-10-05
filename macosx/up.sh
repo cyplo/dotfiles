36aff49a9d479780f79dcd11b24af9cc95655c5e
Cyryl Płotnicki <cyplo@cyplo.net>
2017-10-05 13:13:41 +0100

    Upgrade to high sierra (#135)
    * Upgrade to high sierra
    
    * install gnupg separately
    
    * new iTerm
    
    * Fix tests
    
    * add a script to fixup xcode tools locally if needed

--- macosx/up.sh
+++ macosx/up.sh
@@ -16,10 +16,12 @@ brew unlink gpg-agent
 brew uninstall gpg-agent
 set -e
 
-brew install --force gnupg2
 brew update
 brew upgrade
 brew cask list | xargs brew cask install --force
+
+brew install --force gnupg2
+brew install reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste
 
 brew install aria2
 brew install \
@@ -28,7 +30,6 @@ brew install \
   mobile-shell \
   the_silver_searcher \
   tmux \
-  reattach-to-user-namespace --with-wrap-pbcopy-and-pbpaste \
   tree \
   watch \
   zsh
@@ -80,7 +81,7 @@ brew cask install appcode
 brew cask install adium
 brew cask install kdiff3
 
-brew services start syncthing
+reattach-to-user-namespace brew services restart syncthing
 
 if ! fgrep /usr/local/bin/zsh /etc/shells; then
   sudo bash -c "echo /usr/local/bin/zsh >> /etc/shells"
