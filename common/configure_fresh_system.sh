a8ab4b628229a01b9635a5f3e1b27d506d87ff41
Cyryl Płotnicki <cyplo@cyplo.net>
2017-11-03 18:30:53 +0000

    Basic rust support in vscode linux (#149)
    * Basic rust support in vscode linux
    
    * Link in keybindings for vscode
    
    * Install clippy

--- common/configure_fresh_system.sh
+++ common/configure_fresh_system.sh
@@ -57,6 +57,7 @@ mkdir -p ~/.config/vdirsyncer/
 ln -vfs "$DIR/.config/vdirsyncer/config" ~/.config/vdirsyncer/
 mkdir -p ~/.config/Code/User
 ln -vfs "$DIR/.config/Code/User/settings.json" ~/.config/Code/User/settings.json
+ln -vfs "$DIR/.config/Code/User/keybindings.json" ~/.config/Code/User/keybindings.json
 
 source ~/.setenv
 
@@ -109,19 +110,8 @@ if [[ -z $NORUST ]]; then
 
     set +e
     cargo install cargo-update rustfmt racer rustsym ripgrep
-
-    cd "$DIR/../"
-    if [[ ! -d alacritty ]]; then
-        git clone https://github.com/jwilm/alacritty.git --recursive
-        cd alacritty
-    else
-        cd alacritty 
-        git pull
-        git submodule update --init --recursive
-    fi
-    rustup override set stable
-    cargo install
-set -e
+    rustup run nightly cargo install clippy
+    set -e
 fi
 
 set +e
