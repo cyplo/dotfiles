da0cfa3005ea97c89a03ff9e586553f4e7cbd984
Cyryl Płotnicki <cyplo@cyplo.net>
2017-12-02 13:25:14 +0000

    More Artful

--- ubuntu/up.sh
+++ ubuntu/up.sh
@@ -5,7 +5,7 @@ set -v
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y --fix-missing install apt-file aptitude aria2 atop cmake curl dkms freecad gajim git glances gnupg2 golang gparted gsmartcontrol intltool kdiff3 keepass2 libatk1.0-dev libbonoboui2-dev libcairo2-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libncurses5-dev libx11-dev libxpm-dev libxt-dev linux-kernel-headers lm-sensors meld mercurial nodejs npm pv python-dev python-pip python3-pip realpath retext ruby-dev silversearcher-ag solaar terminator tmux vim whois zsh dirmngr fail2ban glances atop syncthing evolution
+sudo apt-get -y --fix-missing install apt-file aptitude aria2 atop cmake curl dkms freecad gajim git glances gnupg2 golang gparted gsmartcontrol intltool kdiff3 keepass2 libatk1.0-dev libbonoboui2-dev libcairo2-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libncurses5-dev libx11-dev libxpm-dev libxt-dev linux-kernel-headers lm-sensors meld mercurial nodejs npm pv python-dev python-pip python3-pip realpath retext ruby-dev silversearcher-ag solaar terminator tmux vim whois zsh dirmngr fail2ban glances atop syncthing evolution docker.io
 
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
@@ -17,8 +17,8 @@ if [[ -z $NO_SYSTEMCTL ]]; then
     sudo systemctl enable fail2ban
     sudo systemctl restart fail2ban
     echo "Enabling Syncthing for $USER"
-    systemctl enable syncthing@$USER.service
-    systemctl restart syncthing@$USER.service
+    sudo systemctl enable syncthing@$USER.service
+    sudo systemctl restart syncthing@$USER.service
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
