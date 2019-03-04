44ffde3604ccfbe61848d14de148599e1bd90dd8
Cyryl Płotnicki <cyplo@cyplo.net>
2019-03-04 12:40:20 +0000

    Conform to the naming strategy in other scripts

--- fedora/up.sh
+++ fedora/up.sh
@@ -16,7 +16,7 @@ fi
 
 sudo dnf -y --best --allowerasing install tmux atop zsh firefox aria2 gajim lm_sensors freecad python3-pip qt5-qtbase-devel qt5-qtwebkit-devel whois curl pv terminator gsmartcontrol python-pip mercurial python3-devel libxslt-devel libjpeg-turbo-devel cmake gtk2-devel intltool gparted glances dkms kernel-devel gimp git xz util-linux-user dnf-automatic kdiff3 yum-utils util-linux-user ncurses-devel zeal qt5-linguist qtkeychain-qt5-devel archivemount keepass splix gutenprint-cups cups-bjnp golang redhat-rpm-config pcsc-lite-devel pcsc-tools pcsc-lite yubico-piv-tool yubikey-personalization-gui xloadimage yp-tools closure-compiler optipng jpegoptim grub2 grub2-efi dracut dracut-tools openssl-devel fail2ban syncthing ansible gnome-tweaks xclip wget wireguard-dkms wireguard-tools lldb python-lldb kdbg zlib-devel libuuid-devel libattr-devel libblkid-devel libselinux-devel libudev-devel parted lsscsi ksh openssl-devel elfutils-libelf-devel libtirpc-devel kernel-devel ffmpeg-libs dnf-plugins-core
 
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 
@@ -25,10 +25,10 @@ sudo dnf config-manager -y --add-repo https://download.docker.com/linux/fedora/d
 sudo dnf install -y docker-ce docker-ce-cli containerd.io
 
 sudo cp -v /etc/dnf/automatic.conf /etc/dnf/automatic.conf.bak
-sudo cp -v $HERE/etc/dnf/automatic.conf /etc/dnf/automatic.conf
-sudo cp -v $HERE/etc/sysctl.d/90_swapiness.conf /etc/sysctl.d/
-sudo cp -v $HERE/etc/sysctl.d/91_inotify_limit.conf /etc/sysctl.d/
-sudo cp -v $HERE/etc/fail2ban/jail.d/01-sshd.conf /etc/fail2ban/jail.d/
+sudo cp -v $DIR/etc/dnf/automatic.conf /etc/dnf/automatic.conf
+sudo cp -v $DIR/etc/sysctl.d/90_swapiness.conf /etc/sysctl.d/
+sudo cp -v $DIR/etc/sysctl.d/91_inotify_limit.conf /etc/sysctl.d/
+sudo cp -v $DIR/etc/fail2ban/jail.d/01-sshd.conf /etc/fail2ban/jail.d/
 sudo ldconfig
 
 set +e
@@ -89,6 +89,6 @@ if [ "$(id -u)" != "0" ]; then
     code --install-extension vscodevim.vim
 fi
 
-HERE="$HERE/../"
-HERE="$HERE" $HERE/common/configure_fresh_system.sh
+DIR="$DIR/../"
+DIR="$DIR" $DIR/common/configure_fresh_system.sh
 
