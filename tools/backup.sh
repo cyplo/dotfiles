fce0bae9c0445ff55b0776b86f2210f054994748
Cyryl Płotnicki <cyplo@cyplo.net>
2019-03-05 17:01:41 +0000

    Use hostname instead of address

--- tools/backup.sh
+++ tools/backup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-restic -p "$HOME/.vault/restic-password-file" -r sftp:fetcher@10.0.0.101:/mnt/data/backup-targets/ --exclude-file "$HOME/.vault/restic-exclude" --verbose backup "$HOME/"
+restic -p "$HOME/.vault/restic-password-file" -r sftp:fetcher@brix:/mnt/data/backup-targets/ --exclude-file "$HOME/.vault/restic-exclude" --verbose backup "$HOME/"
