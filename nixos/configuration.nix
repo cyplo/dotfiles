97a0d090864b02fc9fd16d06d351cd67acc5b404
Cyryl Płotnicki <cyplo@cyplo.net>
2019-04-07 21:16:16 +0100

    Add hugo

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,7 +38,7 @@ in
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "scanner" "lp" "docker" "vboxusers"]; 
       packages = with pkgs; [
-       firefox chromium terminator zsh keepass fontconfig go nodejs unstable.rustup gcc gdb binutils xclip pkgconfig veracrypt gitAndTools.diff-so-fancy gnome3.gnome-shell-extensions chrome-gnome-shell gnomeExtensions.clipboard-indicator gnomeExtensions.caffeine gnomeExtensions.no-title-bar unstable.gnomeExtensions.gsconnect unstable.appimage-run openjdk10 pdftk pdfshuffler gimp restic glxinfo unstable.discord steam vscode unstable.zoom-us
+       firefox chromium terminator zsh keepass fontconfig go nodejs unstable.rustup gcc gdb binutils xclip pkgconfig veracrypt gitAndTools.diff-so-fancy gnome3.gnome-shell-extensions chrome-gnome-shell gnomeExtensions.clipboard-indicator gnomeExtensions.caffeine gnomeExtensions.no-title-bar unstable.gnomeExtensions.gsconnect unstable.appimage-run openjdk10 pdftk pdfshuffler gimp restic glxinfo unstable.discord steam vscode unstable.zoom-us python36Packages.pylint nodejs-10_x unstable.hugo
       ];
     uid = 1000;
     shell = pkgs.zsh;
