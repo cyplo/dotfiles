501d976d1bbc286fb2359d592897d603d7beb6b8
Cyryl Płotnicki <cyplo@cyplo.net>
2019-04-12 11:11:55 +0100

    Use alacritty instead of terminator

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,7 +42,7 @@ in
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "scanner" "lp" "docker" "vboxusers"]; 
       packages = with pkgs; [
-        firefox chromium terminator zsh keepass fontconfig go nodejs rustup gcc gdb 
+        firefox chromium alacritty zsh keepass fontconfig go nodejs rustup gcc gdb 
         binutils xclip pkgconfig veracrypt gitAndTools.diff-so-fancy
         gnome3.gnome-shell-extensions chrome-gnome-shell gnomeExtensions.clipboard-indicator 
         gnomeExtensions.caffeine gnomeExtensions.no-title-bar gnomeExtensions.gsconnect
