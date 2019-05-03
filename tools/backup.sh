7bcb720ae91d00f00e0d6dec83a44b2be02b2c54
Cyryl Płotnicki <cyplo@cyplo.net>
2019-05-03 11:11:03 +0100

    Make backup play nice

--- tools/backup.sh
+++ tools/backup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-restic -p "$HOME/.vault/restic-password-file" -r sftp:fetcher@brix:/mnt/data/backup-targets/ --exclude-file "$HOME/.vault/restic-exclude" --verbose backup "$HOME/"
+benice restic -p "$HOME/.vault/restic-password-file" -r sftp:fetcher@brix:/mnt/data/backup-targets/ --exclude-file "$HOME/.vault/restic-exclude" --verbose backup "$HOME/"
