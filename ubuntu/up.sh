568060d7a60a089aea2a771d555bb4d7da1827dd
Cyryl Płotnicki <cyplo@cyplo.net>
2019-05-23 13:43:20 +0100

    Remove additional ubuntu software

--- ubuntu/up.sh
+++ ubuntu/up.sh
@@ -5,17 +5,13 @@ set -v
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y --fix-missing install apt-file aptitude aria2 atop cmake curl git glances gnupg2 keepass2 mercurial pv python-dev python-pip python3-pip ruby-dev tmux vim whois zsh dirmngr fail2ban syncthing net-tools coreutils xclip wget
+sudo apt -y --fix-missing install apt-file aptitude aria2 atop cmake curl git glances gnupg2 keepass2 mercurial pv python-dev python-pip python3-pip ruby-dev tmux vim whois zsh dirmngr syncthing net-tools coreutils xclip wget
 
 if [[ -z $USER ]]; then
     USER=`whoami`
 fi
 
 sudo usermod -aG docker $USER
-
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-sudo apt-get -y install syncthing
 
 if [[ -z $NO_SYSTEMCTL ]]; then
     sudo systemctl enable docker
