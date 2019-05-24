faa2617e867ef66390cc70edea5b2942473931c4
Cyryl Płotnicki <cyplo@cyplo.net>
2019-05-24 11:08:02 +0100

    add flatpak

--- ubuntu/up.sh
+++ ubuntu/up.sh
@@ -5,7 +5,9 @@ set -v
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y --fix-missing install apt-file aptitude aria2 atop cmake curl git glances gnupg2 keepass2 mercurial pv python-dev python-pip python3-pip ruby-dev tmux vim whois zsh dirmngr syncthing net-tools coreutils xclip wget scdaemon
+sudo apt -y --fix-missing install apt-file aptitude aria2 atop cmake curl git glances gnupg2 keepass2 mercurial pv python-dev python-pip python3-pip ruby-dev tmux vim whois zsh dirmngr syncthing net-tools coreutils xclip wget scdaemon flatpak gnome-software-plugin-flatpak
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # gsconnect
 sudo ufw allow 1714:1764/udp
