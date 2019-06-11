acda71b908839d7aba9e7a833b0d80a677f3f2eb
Cyryl Płotnicki <cyplo@cyplo.net>
2019-06-11 10:51:44 +0100

    Add TLP to Ubuntu

--- ubuntu/up.sh
+++ ubuntu/up.sh
@@ -5,7 +5,7 @@ set -v
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y --fix-missing install apt-file aptitude aria2 atop cmake curl git glances gnupg2 keepass2 mercurial pv python-dev python-pip python3-pip ruby-dev tmux vim whois zsh dirmngr syncthing net-tools coreutils xclip wget scdaemon flatpak gnome-software-plugin-flatpak ufw
+sudo apt -y --fix-missing install apt-file aptitude aria2 atop cmake curl git glances gnupg2 keepass2 mercurial pv python-dev python-pip python3-pip ruby-dev tmux vim whois zsh dirmngr syncthing net-tools coreutils xclip wget scdaemon flatpak gnome-software-plugin-flatpak ufw tlp
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -22,8 +22,8 @@ sudo groupadd docker | true
 sudo usermod -aG docker $USER
 
 if [[ -z $NO_SYSTEMCTL ]]; then
-    sudo systemctl enable docker
-    sudo systemctl restart docker
+    sudo systemctl enable --now tlp
+    sudo systemctl enable --now docker
     sudo systemctl enable --now syncthing@$USER.service
 fi
 
