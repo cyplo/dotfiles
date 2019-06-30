db498d3cd7272116020968fc533eb0577aef350f
Cyryl Płotnicki <cyplo@cyplo.net>
2019-06-30 16:29:48 +0100

    More zsh config

--- nixos/home.nix
+++ nixos/home.nix
@@ -6,11 +6,22 @@
     home-manager.enable = true;
     zsh = { 
       enable = true;
-      history.share = true;
+      history = { 
+        size = 102400;
+        save = 102400;
+        ignoreDups = true;
+        expireDuplicatesFirst = true;
+        extended = true;
+        share = true; 
+      };
+      enableAutosuggestions = true;
+      enableCompletion = true;
       oh-my-zsh = {
         enable = true;
         theme = "agnoster";
       };
+      sessionVariables = { EDITOR="vim"; VISUAL="vim"; PAGER="less"; };
+      shellAliases = { tmate = "tmux detach-client -E 'tmate;tmux'"; };
     };
     firefox.enable = true;
     chromium.enable = true;
