84ad5af5081f623a4eeae17a21cd1c1a03a5d125
Cyryl Płotnicki <cyplo@cyplo.net>
2019-07-21 11:40:02 +0100

    Add cachix

--- nixos/common.nix
+++ nixos/common.nix
@@ -13,6 +13,16 @@ in
         ./common-hardware.nix
       ];
 
+      nix = {
+        binaryCaches = [
+          "https://cyplo.cachix.org"
+        ];
+        binaryCachePublicKeys = [
+          "cyplo.cachix.org-1:6cmlYl6tQ1eidsjU2tw6uGN2hmzQ+P5uiQhuo6d1P5o="
+        ];
+
+      };
+
       nixpkgs.config = {
         allowUnfree = true;
         packageOverrides = pkgs: {
