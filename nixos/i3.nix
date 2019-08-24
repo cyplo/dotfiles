a66793965db7dad4080567a154854a42124683bf
Cyryl Płotnicki <cyplo@cyplo.net>
2019-08-24 11:56:02 +0100

    Add workspace shortcuts in i3

--- nixos/i3.nix
+++ nixos/i3.nix
@@ -54,6 +54,17 @@ in
           "${mod}+j"   = "focus down";
           "${mod}+k"   = "focus up";
           "${mod}+l"   = "focus right";
+
+          "${mod}+1" = "workspace 1";
+          "${mod}+2" = "workspace 2";
+          "${mod}+3" = "workspace 3";
+          "${mod}+4" = "workspace 4";
+          "${mod}+5" = "workspace 5";
+          "${mod}+6" = "workspace 6";
+          "${mod}+7" = "workspace 7";
+          "${mod}+8" = "workspace 8";
+          "${mod}+9" = "workspace 9";
+          "${mod}+0" = "workspace 10";
         };
       };
     };
