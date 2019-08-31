4eb42e68feae3d3b23af25e4b03e367db8238701
Cyryl Płotnicki <cyplo@cyplo.net>
2019-08-31 11:39:06 +0100

    emoji support

--- nixos/common.nix
+++ nixos/common.nix
@@ -48,7 +48,7 @@ in
         autoPrune.enable = true;
       };
 
-      fonts.fonts = [ pkgs.powerline-fonts pkgs.weather-icons pkgs.material-icons pkgs.source-code-pro pkgs.fira-code ];
+      fonts.fonts = with pkgs; [ powerline-fonts weather-icons material-icons source-code-pro fira-code noto-fonts-emoji emojione ];
 
       nix = {
         autoOptimiseStore = true;
