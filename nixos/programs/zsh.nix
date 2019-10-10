e3f903f3d041902389141bd61bfde7b47d397650
Cyryl Płotnicki <cyplo@cyplo.net>
2019-10-10 21:32:21 +0100

    Remove incompatible plugins

--- nixos/programs/zsh.nix
+++ nixos/programs/zsh.nix
@@ -1,31 +1,31 @@
 { config, pkgs, ... }:
 
 {
-    programs.zsh = { 
+  programs.zsh = {
+    enable = true;
+    history = {
+      size = 102400;
+      save = 102400;
+      ignoreDups = true;
+      expireDuplicatesFirst = true;
+      share = true;
+    };
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    oh-my-zsh = {
       enable = true;
-      history = { 
-        size = 102400;
-        save = 102400;
-        ignoreDups = true;
-        expireDuplicatesFirst = true;
-        share = true; 
-      };
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "vi-mode" "git" "python" "syntax-highlighting" "history-substring-search" "spectrum" "sshi" "prompt" "gpg" "autosuggestions" "tmux" ];
-      };
-	  plugins = [
-		  {
-			name = "spaceship";
-			file = "spaceship.zsh";
-			src = pkgs.fetchgit {
-			  url = "https://github.com/denysdovhan/spaceship-prompt";
-			  rev = "v3.11.1";
-			  sha256 = "0habry3r6wfbd9xbhw10qfdar3h5chjffr5pib4bx7j4iqcl8lw8";
-			};
-	  }];
+      plugins = [ "vi-mode" "git" "python" "history-substring-search" "tmux" ];
+    };
+    plugins = [
+      {
+        name = "spaceship";
+        file = "spaceship.zsh";
+        src = pkgs.fetchgit {
+          url = "https://github.com/denysdovhan/spaceship-prompt";
+          rev = "v3.11.1";
+          sha256 = "0habry3r6wfbd9xbhw10qfdar3h5chjffr5pib4bx7j4iqcl8lw8";
+        };
+      }];
       initExtra = ''
         SPACESHIP_TIME_SHOW=true
         SPACESHIP_EXIT_CODE_SHOW=true
@@ -33,7 +33,7 @@
         SPACESHIP_BATTERY_THRESHOLD=30
         ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=238'
         setopt HIST_IGNORE_ALL_DUPS
-        '';
+      '';
       profileExtra = ''
         export PATH="$HOME/programs:$PATH";
         export PATH="$HOME/tools:$PATH";
@@ -43,9 +43,9 @@
         export PATH="$HOME/.rvm/bin:$PATH";
         export PATH="$HOME/.cargo/bin:$PATH";
         export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH";
-        '';
-      sessionVariables = { 
-        TERM="xterm-256color"; 
+      '';
+      sessionVariables = {
+        TERM="xterm-256color";
         EDITOR="vim";
         VISUAL="vim";
         PAGER="less";
