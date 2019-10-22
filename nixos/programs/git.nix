a1ccdb50f5f43616c117451e01d2a00c4a3414cf
Cyryl Płotnicki <cyplo@cyplo.net>
2019-10-22 11:57:34 +0100

    Encode git config in home manager

--- nixos/programs/git.nix
+++ nixos/programs/git.nix
@@ -4,8 +4,18 @@
     enable = true;
     userName = "Cyryl Płotnicki";
     userEmail = "cyplo@cyplo.net";
-    includes = [
-      { path =    "../.gitconfig.linux.private"; }
-    ];
-  };
-}
+    extraConfig = {
+      core = { pager = "cat"; };
+      pager = {
+        diff = "diff-so-fancy | less --tabs=1,5 -RFX";
+        show = "diff-so-fancy | less --tabs=1,5 -RFX";
+      };
+    };
+    aliases =
+      {
+        tree = "log --show-signature --color --decorate --date=short --all --graph -n 3";
+        newbranch = "!git checkout master && git fetch -p && git reset --hard origin/master && git checkout -b $2";
+        head = "log HEAD -n1";
+      };
+    };
+  }
