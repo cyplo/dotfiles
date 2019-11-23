d38f417be849c7838e6edc14659bcc2a5a668110
Cyryl Płotnicki <cyplo@cyplo.net>
2019-11-23 09:27:28 +0000

    add missing cli utils onnon-nixos

--- nixos/cli.nix
+++ nixos/cli.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+    wget git gnupg curl tmux python36Packages.glances htop atop pciutils powertop ripgrep-all fd dnsutils
     ( pass.withExtensions (ext: [ ext.pass-otp ext.pass-import ext.pass-genphrase ext.pass-audit ext.pass-update ]))
     cabal-install stack hsetroot lm_sensors
     wirelesstools ranger apvlv
