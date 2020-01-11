a9bb8afe77645816375dcaa5b516ae1a37fba8bb
Cyryl Płotnicki <cyplo@cyplo.net>
2020-01-11 14:13:01 +0000

    Add dunst color settings

--- nixos/i3/dunst.nix
+++ nixos/i3/dunst.nix
@@ -2,5 +2,29 @@
 {
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        frame_color = "#93a1a1";
+        separator_color = "#93a1a1";
+      };
+
+      low = {
+        msg_urgency = "low";
+        background = "#073642";
+        foreground = "#657b83";
+      };
+
+      normal = {
+        msg_urgency = "normal";
+        background = "#586e75";
+        foreground = "#93a1a1";
+      };
+
+      critical = {
+        msg_urgency = "critical";
+        background = "#dc322f";
+        foreground = "#eee8d5";
+      };
+    };
   };
 }
