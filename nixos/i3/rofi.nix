cd477dec296b6b96a27f44409926b7f92cf22acd
Cyryl Płotnicki <cyplo@cyplo.net>
2020-02-15 09:23:48 +0000

    Use official solarized theme for rofi

--- nixos/i3/rofi.nix
+++ nixos/i3/rofi.nix
@@ -6,24 +6,6 @@
     font = "Fira Code 12";
     separator = "solid";
     scrollbar = false;
-    colors = {
-      window = {
-        background = "#002b36";
-        border = "#073642";
-        separator = "#002b36";
-      };
-
-      rows = {
-        normal = {
-          background = "#002b36";
-          foreground = "#839496";
-          backgroundAlt = "argb:58455a64";
-          highlight = {
-            background = "#002b36";
-            foreground = "#fafbfc";
-          };
-        };
-      };
-    };
+    theme = "solarized";
   };
 }
