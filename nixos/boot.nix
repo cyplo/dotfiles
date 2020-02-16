138bef407d4af04ae697aa4ae7477d6f637402f9
Cyryl Płotnicki <cyplo@cyplo.net>
2020-02-16 10:15:11 +0000

    allow for truecrypt

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
 {
   fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+  security.lockKernelModules = false;
 
   boot = {
-    kernelModules = [ "acpi_call" ];
-    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
     kernel.sysctl = {
       "vm.swappiness" = 1;
       "max_user_watches" = 524288;
