161b9fb141c956c4b2eb5510f1469a975f0aaee0
Cyryl Płotnicki <cyplo@cyplo.net>
2020-02-18 19:26:04 +0000

    remove deprecated style

--- nixos/boxes/foureighty.nix
+++ nixos/boxes/foureighty.nix
@@ -5,40 +5,40 @@
   boot = {
     kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest_hardened;
 
-    initrd.luks.devices = [
-      {
-        name = "root";
+    initrd.luks.devices = {
+      root = {
         device = "/dev/disk/by-uuid/a9e8a44f-15be-4844-a0a1-46892cc5e44e";
         preLVM = true;
         allowDiscards = true;
-      }];
-      loader.grub = {
-        device = "nodev";
-        efiSupport = true;
       };
-      loader.efi.canTouchEfiVariables = true;
     };
-
-    time.hardwareClockInLocalTime = true;
-    time.timeZone = "Europe/London";
-
-    hardware.trackpoint.enable = true;
-    services.hardware.bolt.enable = true;
-    services.fprintd = {
-      enable = true;
-      package = pkgs.fprintd-thinkpad;
+    loader.grub = {
+      device = "nodev";
+      efiSupport = true;
     };
+    loader.efi.canTouchEfiVariables = true;
+  };
 
-    hardware.nvidiaOptimus.disable = true;
-    hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
+  time.hardwareClockInLocalTime = true;
+  time.timeZone = "Europe/London";
 
-    imports = [
-      /etc/nixos/hardware-configuration.nix
-      ../boot.nix
-      ../common.nix
-      ../gfx-intel.nix
-      ../virtualbox.nix
-      ../zerotier.nix
-      ../i3/system.nix
-    ];
-  }
+  hardware.trackpoint.enable = true;
+  services.hardware.bolt.enable = true;
+  services.fprintd = {
+    enable = true;
+    package = pkgs.fprintd-thinkpad;
+  };
+
+  hardware.nvidiaOptimus.disable = true;
+  hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
+
+  imports = [
+    /etc/nixos/hardware-configuration.nix
+    ../boot.nix
+    ../common.nix
+    ../gfx-intel.nix
+    ../virtualbox.nix
+    ../zerotier.nix
+    ../i3/system.nix
+  ];
+}
