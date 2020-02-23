839b084eebd86572c6ac2e5fc3c8d0aac0f1b609
Cyryl Płotnicki <cyplo@cyplo.net>
2020-02-23 20:17:37 +0000

    fine tube distributed build

--- nixos/distributed-builds.nix
+++ nixos/distributed-builds.nix
@@ -2,21 +2,21 @@
 {
   nix.buildMachines = [
     {
-      hostName = "brix.local";
-      sshUser = "nix-builder";
-      sshKey = "/home/cyryl/.ssh/id_ed25519";
-      system = "x86_64-linux";
-      maxJobs = 1;
-      supportedFeatures = [ "kvm" ];
-      mandatoryFeatures = [ ];
-    }
-    {
       hostName = "vultr1.local";
       sshUser = "nix-builder";
       sshKey = "/home/cyryl/.ssh/id_ed25519";
       system = "x86_64-linux";
       maxJobs = 2;
       supportedFeatures = [ ];
+      mandatoryFeatures = [ ];
+    }
+    {
+      hostName = "brix.local";
+      sshUser = "nix-builder";
+      sshKey = "/home/cyryl/.ssh/id_ed25519";
+      system = "x86_64-linux";
+      maxJobs = 1;
+      supportedFeatures = [ "kvm" ];
       mandatoryFeatures = [ ];
     }
   ];
