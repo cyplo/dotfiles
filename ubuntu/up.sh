dbbcd08e35ff27c0cc3527e151962fdb2f15ff4d
Cyryl Płotnicki <cyryl.plotnicki@form3.tech>
2020-04-18 12:11:27 +0100

    move ubuntu home manager to 20.03

--- ubuntu/up.sh
+++ ubuntu/up.sh
@@ -14,7 +14,7 @@ curl https://nixos.org/nix/install | sh
 source /home/cyryl/.nix-profile/etc/profile.d/nix.sh
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 
-nix-channel --add https://github.com/rycee/home-manager/archive/release-19.09.tar.gz home-manager
+nix-channel --add https://github.com/rycee/home-manager/archive/release-20.03.tar.gz home-manager
 nix-channel --update
 
 nix-shell '<home-manager>' -A install
