1b7681e5b2caad9538989f6a6a47d591151003b8
Cyryl Płotnicki <cyplo@cyplo.net>
2020-07-25 11:33:13 +0100

    add vacuum

--- nixos/home-manager/programs/git.nix
+++ nixos/home-manager/programs/git.nix
@@ -18,6 +18,7 @@
         tree = "log --show-signature --color --decorate --date=short --all --graph -n 3";
         newbranch = "!git checkout master && git fetch -p && git reset --hard origin/master && git checkout -b $2";
         head = "log HEAD -n1";
+        vacuum = "!git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs -r git branch -D && git gc --aggressive --auto";
       };
     };
   }
