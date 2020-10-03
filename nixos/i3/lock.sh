9fe5a37194dc374f25b7c11c077531e095208261
Cyryl Płotnicki <cyplo@cyplo.net>
2020-10-03 20:13:27 +0100

    port to newer xidlehook

--- nixos/i3/lock.sh
+++ nixos/i3/lock.sh
@@ -7,9 +7,16 @@ export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
 xset s off
 xset -dpms
+
 xidlehook \
     --not-when-fullscreen \
     --not-when-audio \
-    --timer normal 120 'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' 'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-    --timer normal 300 'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; systemctl suspend' 'xrandr --output "$PRIMARY_DISPLAY" --brightness 1'
-
+    --timer 60 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+    --timer 10 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; physlock -d' \
+    '' \
+    --timer 600 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; systemctl suspend' \
+    ''
