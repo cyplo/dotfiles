b405a963bb3f4402338659f6612fa5fe77ba8285
Cyryl Płotnicki <cyplo@cyplo.dev>
2021-04-05 20:35:36 +0100

    Add basic guix config

---
+++ guix/skinnyv/config.scm
@@ -0,0 +1,92 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+
+(use-modules (gnu))
+(use-modules (nongnu packages linux)
+             (nongnu system linux-initrd))
+(use-modules (gnu packages version-control))
+(use-modules (gnu packages gnuzilla))
+(use-modules (gnu packages vim))
+(use-modules (gnu packages ssh))
+
+(use-service-modules desktop networking ssh xorg)
+
+(define %xorg-libinput-config
+  "Section \"InputClass\"
+  Identifier \"Touchpads\"
+  Driver \"libinput\"
+  MatchDevicePath \"/dev/input/event*\"
+  MatchIsTouchpad \"on\"
+
+  Option \"Tapping\" \"on\"
+  Option \"TappingDrag\" \"on\"
+  Option \"DisableWhileTyping\" \"on\"
+  Option \"MiddleEmulation\" \"on\"
+  Option \"ScrollMethod\" \"twofinger\"
+EndSection
+Section \"InputClass\"
+  Identifier \"Keyboards\"
+  Driver \"libinput\"
+  MatchDevicePath \"/dev/input/event*\"
+  MatchIsKeyboard \"on\"
+EndSection
+")
+
+(operating-system
+  (locale "en_GB.utf8")
+  (timezone "Europe/London")
+  (keyboard-layout (keyboard-layout "pl"))
+  (host-name "skinnyv")
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (list linux-firmware))
+  (users (cons* (user-account
+                  (name "cyryl")
+                  (comment "")
+                  (group "users")
+                  (home-directory "/home/cyryl")
+                  (supplementary-groups
+                    '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+  (packages
+    (append
+      (list (specification->package "i3-wm")
+            (specification->package "i3status")
+            (specification->package "dmenu")
+            (specification->package "st")
+            (specification->package "nss-certs")
+	git
+	icecat
+	vim
+	openssh)
+      %base-packages))
+  (services
+    (append
+      (list (service gnome-desktop-service-type)
+            (set-xorg-configuration
+              (xorg-configuration
+                (keyboard-layout keyboard-layout)
+                (extra-config (list %xorg-libinput-config)))))
+      %desktop-services))
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (target "/boot/efi")
+      (keyboard-layout keyboard-layout)))
+  (mapped-devices
+    (list (mapped-device
+            (source
+              (uuid "d47f7041-717d-4e6d-a3c4-f2f02cb39c1f"))
+            (target "cryptroot")
+            (type luks-device-mapping))))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "5B87-265F" 'fat32))
+             (type "vfat"))
+           (file-system
+             (mount-point "/")
+             (device "/dev/mapper/cryptroot")
+             (type "ext4")
+             (dependencies mapped-devices))
+           %base-file-systems)))
