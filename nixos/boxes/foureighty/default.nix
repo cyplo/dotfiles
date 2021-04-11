8cb8361646d3c8ba700f128d11d8cf665d2c4aa7
Cyryl Płotnicki <cyplo@cyplo.net>
2021-04-11 10:45:15 +0100

    fix copypasta

--- nixos/boxes/foureighty/default.nix
+++ nixos/boxes/foureighty/default.nix
@@ -19,11 +19,11 @@
   ];
 
   boot.kernelPackages = pkgs.unstable.linuxPackages_latest_hardened;
-  loader.grub = {
-   enable = true;
-   version = 2;
-   useOSProber = true;
-   memtest86.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    useOSProber = true;
+    memtest86.enable = true;
   };
   time.hardwareClockInLocalTime = true;
   time.timeZone = "Europe/London";
