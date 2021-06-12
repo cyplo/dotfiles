36183c5059cf92fe1d72979580c563ce55dabcef
Cyryl Płotnicki <cyplo@cyplo.net>
2021-06-12 12:54:16 +0100

    compress btrfs on skinny

--- nixos/boxes/brix/brix-boot.nix
+++ nixos/boxes/brix/brix-boot.nix
@@ -12,25 +12,25 @@
   };
 
   services.btrfs.autoScrub.enable = true;
-  fileSystems."/data" =
-    { device = "/dev/disk/by-uuid/78e8e5b5-9068-4381-8e85-b4297607f9ea";
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/78e8e5b5-9068-4381-8e85-b4297607f9ea";
     fsType = "btrfs";
-    options = [ "autodefrag" "space_cache" "inode_cache" "noatime" "nodiratime" ];
+    options = [ "autodefrag" "space_cache" "inode_cache" "noatime" "nodiratime" "compress=zstd" ];
   };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/68bb21bd-90da-4da4-b97e-c6da3b1f8235";
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/68bb21bd-90da-4da4-b97e-c6da3b1f8235";
     fsType = "ext4";
     options = [ "noatime" "nodiratime" ];
   };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0129-8152";
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/0129-8152";
     fsType = "vfat";
   };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/5f635052-a940-466e-a7cf-4799adace60e"; }
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/5f635052-a940-466e-a7cf-4799adace60e"; }
   ];
 
   zramSwap = {
