db9bfb5cd20ae17f0022da0081fccf25482a5639
Cyryl Płotnicki <cyplo@cyplo.net>
2021-06-26 22:15:36 +0100

    update syncthing

--- nixos/syncthing.nix
+++ nixos/syncthing.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 {
   services.syncthing = {
     enable = true;
     user = "cyryl";
     dataDir = "/home/cyryl/.syncthing";
     openDefaultPorts = true;
+    package = inputs.nixpkgs-nixos-unstable.legacyPackages."x86_64-linux".syncthing;
   };
 }
